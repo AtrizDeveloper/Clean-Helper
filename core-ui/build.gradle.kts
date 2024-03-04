a7bf0a69b3d0614cf8a83c303cb0771ceef52468
Jair Muñoz <jair.munoz.glez@gmail.com>
2024-03-04 14:08:06 -0600

    UI helpers

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -46,7 +46,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "mx.com.atriz"
             artifactId = "core-ui"
-            version = "0.0.1"
+            version = "0.0.2"
             afterEvaluate {
                 from(components["release"])
             }
